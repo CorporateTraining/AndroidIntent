c1da2992bfc151409d33c3c4f32866840b575ece
mahuanfy <mahuanfy@foxmail.com>
2020-07-21 10:30:25 +0800

    [Huanhuan]: refactor the code.

--- app/src/main/java/com/example/androidintent/MainActivity.java
+++ app/src/main/java/com/example/androidintent/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void openContactActivity(View view){
-        Intent intent = new Intent(this, ContactActivity.class);
-        startActivity(intent);
+        Button constraintLayout = findViewById(R.id.activity_practices);
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
